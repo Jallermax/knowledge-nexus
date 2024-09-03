11f41b90144bd738bed68eb9dbfb7fe6bcc0653c
Jallermax <jallermax@gmail.com>
2024-09-03 06:19:47 +0400

    Add env configurable logging level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import dotenv
 
@@ -7,10 +8,12 @@ from graph_rag.processor import ContentChunkerAndEmbedder
 from graph_rag.processor.graph_builder import GraphBuilder
 from graph_rag.storage import Neo4jManager
 from graph_rag.data_source import NotionProvider
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from graph_rag.pipeline import DataProcessingPipeline
 
 dotenv.load_dotenv()
+
+log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
+logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def main():
